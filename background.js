c9f8e62f029fcd7fd0044efafdfbf8ca3e11932a
Dan Wallis <mrdanwallis@gmail.com>
2017-08-07 22:27:08 +1200

    Adding support for Chrome, and version bump

--- background.js
+++ background.js
@@ -1,27 +1,34 @@
 // Note that this does not work with HSTS. Those URLs will always stay
 // not-updated, as that's not an explicit 301 from the bookmarked page.
+(function() {
+  'use strict';
 
-function no_op() {}
+  function no_op() {}
 
-function process301(responseDetails) {
-  if (301 == responseDetails.statusCode) {
-    updateBookmarks(responseDetails.url, responseDetails.redirectUrl);
+  function process301(responseDetails) {
+    if (301 == responseDetails.statusCode) {
+      updateBookmarks(responseDetails.url, responseDetails.redirectUrl);
+    }
   }
-}
 
-function updateBookmarks(old_url, new_url) {
-  browser.bookmarks.search({url: old_url})
-  .then(function(bookmarkItems) {
-    for (item of bookmarkItems) {
-      browser.bookmarks.update(item.id, {
-        url: new_url
+  function updateBookmarks(old_url, new_url) {
+    chrome.bookmarks.search({url: old_url}, updateBookmarksCallback)
+    .then(updateBookmarksCallback, no_op);
+
+    function updateBookmarksCallback(bookmarkItems) {
+      for (let item of bookmarkItems) {
+        chrome.bookmarks.update(item.id, {
+          url: new_url
         })
-      .then(no_op, no_op);
+        .then(no_op, no_op);
       }
-  }, no_op);
-}
+    }
 
-chrome.webRequest.onBeforeRedirect.addListener(
-  process301,
-  {urls: ["<all_urls>"]}
-);
+  }
+
+  chrome.webRequest.onBeforeRedirect.addListener(
+    process301,
+    {urls: ["<all_urls>"]}
+  );
+
+})();
